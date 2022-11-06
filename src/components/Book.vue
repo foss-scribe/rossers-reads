95840c9a64ccfc6ff6499263c4c1318d358c30a5
Chris Rosser <chris@chrisrosser.net>
2022-11-06 11:36:32 +1100

    adds book component, top list, and updates some data

---
+++ src/components/Book.vue
@@ -0,0 +1,30 @@
+<template>
+    <img v-if="book.cover" :src="book.cover" :alt="book.title" class="border-round-left" style="max-height: 300px;">
+    <div class="p-4">
+        <div class="font-bold text-2xl text-900 line-height-3 ">{{book.title}}</div>
+        <em><span v-if="book.series">{{book.series}}</span><span v-if="book.series_no" > book {{book.series_no}}</span></em>
+        <div class="font-medium text-sm block mt-1 mb-3">By {{book.author}} 
+            
+        </div>
+        <div class="line-height-3 text-700 text-sm mb-3">{{book.description}}</div>
+        <div class="flex flex-column">
+            <div class="flex-none">Reading on {{book.medium}}</div>
+            <div class="flex-grow-1 flex align-items-center">
+                <a v-if="book.amazon" :href="book.amazon"><i  class="pi pi-amazon"></i></a>
+            </div>
+        </div>
+        <div class="mt-1">
+            <span v-if="book.pages">{{book.pages}} pages </span>
+            <span v-if="book.genre">| {{book.genre}}</span>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Book",
+    props: ['book']
+
+}
+</script>
--- src/components/TheBookList.vue
+++ src/components/TheBookList.vue
@@ -13,57 +13,37 @@
             <span class="font-medium text-500">Read</span>
             <div class="text-700 mt-2">{{readList.length}}</div>
         </div>
+        <div class="mr-5 mt-3 cursor-pointer" @click="toggleTopList">
+            <span class="font-medium text-500">Top</span>
+            <div class="text-700 mt-2">{{topList.length}}</div>
+        </div>
     </div>
 
     <h2 class="cursor-pointer" @click="toggleReadingNow">Reading now</h2>
     <div v-if="showReadingList" class="flex flex-column">
         <div v-for="book in readingList" class="flex shadow-2 surface-card border-round mr-0 xl:mr-4 mb-6 flex-row">
-            <img v-if="book.cover" :src="book.cover" :alt="book.title" class="border-round-left" style="max-height: 300px;">
-            <div class="p-4">
-                <div class="font-bold text-2xl text-900 line-height-3 ">{{book.title}}</div>
-                <em><span v-if="book.series">{{book.series}}</span><span v-if="book.series_no" > book {{book.series_no}}</span></em>
-                <div class="font-medium text-sm block mt-1 mb-3">By {{book.author}} 
-                    
-                </div>
-                <div class="line-height-3 text-700 text-sm mb-3">{{book.description}}</div>
-                <div class="flex flex-column">
-                    <div class="flex-none">Reading on {{book.medium}}</div>
-                    <div class="flex-grow-1 flex align-items-center">
-                        <a v-if="book.amazon" :href="book.amazon"><i  class="pi pi-amazon"></i></a>
-                    </div>
-                </div>
-                <div class="mt-1">
-                    <span v-if="book.pages">{{book.pages}} pages </span>
-                    <span v-if="book.genre">| {{book.genre}}</span>
-                </div>
-                
-            </div>
+            <Book :book="book"/>
         </div>
     </div>
 
     <h2 class="cursor-pointer" @click="toggleWantToRead">Want to read</h2>
     <div v-if="showToReadList" class="flex flex-column">
         <div v-for="book in toReadList" class="flex shadow-2 surface-card border-round mr-0 xl:mr-4 mb-6 flex-row">
-            <img v-if="book.cover" :src="book.cover" :alt="book.title" class="border-round-left" style="max-height: 234px;">
-            <div class="p-4">
-                <div class="font-bold text-900 mb-3 line-height-3 ">{{book.title}}</div>
-                <div class="font-medium text-sm block">{{book.author}}</div>
-                <div class="line-height-3 text-700 text-sm mb-3">{{book.description}}</div>
-                <div class="flex align-items-center"></div>
-            </div>
+            <Book :book="book"/>
         </div>
     </div>
 
     <h2 class="cursor-pointer" @click="toggleReadList">Read</h2>
     <div v-if="showReadList" class="flex flex-column">
         <div v-for="book in readList" class="flex shadow-2 surface-card border-round mr-0 xl:mr-4 mb-6 flex-row">
-            <img v-if="book.cover" :src="book.cover" :alt="book.title" class="border-round-left" style="max-height: 234px;">
-            <div class="p-4">
-                <div class="font-bold text-900 mb-3 line-height-3 ">{{book.title}}</div>
-                <div class="font-medium text-sm block">{{book.author}}</div>
-                <div class="line-height-3 text-700 text-sm mb-3">{{book.description}}</div>
-                <div class="flex align-items-center"></div>
-            </div>
+            <Book :book="book"/>
+        </div>
+    </div>
+
+    <h2 class="cursor-pointer" @click="toggleTopList">Top Rated</h2>
+    <div v-if="showTopList" class="flex flex-column">
+        <div v-for="book in topList" class="flex shadow-2 surface-card border-round mr-0 xl:mr-4 mb-6 flex-row">
+            <Book :book="book"/>
         </div>
     </div>
 
@@ -72,18 +52,22 @@
 <script>
 
 import booklist from '../data/booklist.json';
+import Book from './Book.vue';
 
 export default {
     name: "TheBookList",
+    components: {Book},
     data() {
         return {
             booklist,
             readingList: [],
             readList: [],
             toReadList: [],
+            topList: [],
             showReadingList: true,
             showReadList: false,
-            showToReadList: false
+            showToReadList: false,
+            showTopList: false
         }
     },
     mounted() {
@@ -97,6 +81,9 @@ export default {
             })
             this.toReadList = this.booklist.filter((item) => {
                 return item.status == 'to-read'
+            })
+            this.topList = this.booklist.filter((item) => {
+                return item.rating == 5
             })
         }
         
@@ -112,6 +99,7 @@ export default {
                 this.showToReadList = true;
                 this.showReadingList = false;
                 this.showReadList = false;
+                this.showTopList = false;
             } else {
                 this.showToReadList = false;
             }
@@ -121,6 +109,7 @@ export default {
                 this.showReadingList = true;
                 this.showToReadList = false;
                 this.showReadList = false;
+                this.showTopList = false;
             } else {
                 this.showReadingList = false;
             }
@@ -130,8 +119,19 @@ export default {
                 this.showReadList = true;
                 this.showReadingList = false;
                 this.showToReadList = false;
+                this.showTopList = false;
             } else {
                 this.showReadList = false;
+            }
+        },
+        toggleTopList() {
+            if (this.showTopList == false) {
+                this.showTopList = true;
+                this.showReadList = false;
+                this.showReadingList = false;
+                this.showToReadList = false;
+            } else {
+                this.showTopList = false;
             }
         }
     }
@@ -139,7 +139,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
 
 .booklist {
     display: flex;
